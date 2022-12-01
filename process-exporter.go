f5fd1ae0f48bfea4e0b12f02ab14dc174afb1005
payne-pro <85580176+payne-pro@users.noreply.github.com>
2022-12-01 21:55:46 +0200

    Test (#1)
    * #test cpu usage
    
    * /change/ add scrape openfiles
    
    * /change/ add kamailio binrpc
    
    * /change/ add kamailio binrpc v2
    
    * /change/ add method for connect and scrape ul.dump kamailio
    
    * ready
    
    * ready
    
    Co-authored-by: Bullet <bullet.ringostat@gmail.com>

--- process-exporter.go
+++ process-exporter.go
@@ -5,21 +5,37 @@ import (
 	//	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/version"
+	"github.com/shirou/gopsutil/process"
+	"github.com/sirupsen/logrus"
+	"net"
+	"net/url"
+	"time"
+
 	//	"io/ioutil"
 	"net/http"
 	//	"net/url"
 	"os"
 	//	"regexp"
+	binrpc "github.com/florentchauveau/go-kamailio-binrpc/v3"
 	"strconv"
 	"strings"
-	//	"time"
-	//
-	"github.com/shirou/gopsutil/process"
-
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/common/log"
-	"github.com/prometheus/common/version"
 )
+
+// MetricValue is the value of a metric, with its labels.
+type MetricValue struct {
+	Value  float64
+	Labels map[string]string
+}
+
+type KamailioMetric struct {
+	Kind   prometheus.ValueType
+	Name   string
+	Help   string
+	Method string // kamailio method associated with the metric
+}
 
 const (
 	namespace = "indivdual"
@@ -28,12 +44,68 @@ const (
 var (
 	to_watch = []string{}
 	to_skip  = []string{}
+
+	metricsList = map[string][]KamailioMetric{
+		"core.shmmem": {
+			NewMetricGauge("total", "Total shared memory.", "core.shmmem"),
+			NewMetricGauge("free", "Free shared memory.", "core.shmmem"),
+			NewMetricGauge("used", "Used shared memory.", "core.shmmem"),
+			NewMetricGauge("real_used", "Real used shared memory.", "core.shmmem"),
+			NewMetricGauge("max_used", "Max used shared memory.", "core.shmmem"),
+			NewMetricGauge("fragments", "Number of fragments in shared memory.", "core.shmmem"),
+		},
+		"core.uptime": {
+			NewMetricCounter("uptime", "Uptime in seconds.", "core.uptime"),
+		},
+		"core.tcp_info": {
+			NewMetricGauge("readers", "Total TCP readers.", "core.tcp_info"),
+			NewMetricGauge("max_connections", "Maximum TCP connections", "core.tcp_info"),
+			NewMetricGauge("max_tls_connections", "Maximum TLS connections.", "core.tcp_info"),
+			NewMetricGauge("opened_connections", "Opened TCP connections.", "core.tcp_info"),
+			NewMetricGauge("opened_tls_connections", "Opened TLS connections.", "core.tcp_info"),
+			NewMetricGauge("write_queued_bytes", "Write queued bytes.", "core.tcp_info"),
+		},
+		"dlg.stats_active": {
+			NewMetricGauge("starting", "Dialogs starting.", "dlg.stats_active"),
+			NewMetricGauge("connecting", "Dialogs connecting.", "dlg.stats_active"),
+			NewMetricGauge("answering", "Dialogs answering.", "dlg.stats_active"),
+			NewMetricGauge("ongoing", "Dialogs ongoing.", "dlg.stats_active"),
+			NewMetricGauge("all", "Dialogs all.", "dlg.stats_active"),
+		},
+		"ul.dump": {
+			NewMetricGauge("users_count", "Count of registered user.", "ul.dump"),
+		},
+	}
 )
+
+func NewMetricGauge(name string, help string, method string, labels ...string) KamailioMetric {
+	return KamailioMetric{
+		prometheus.GaugeValue,
+		name,
+		help,
+		method,
+	}
+}
+
+// NewMetricCounter is a helper function to create a counter.
+func NewMetricCounter(name string, help string, method string, labels ...string) KamailioMetric {
+	return KamailioMetric{
+		prometheus.CounterValue,
+		name,
+		help,
+		method,
+	}
+}
 
 type Exporter struct {
 	up             prometheus.Gauge
 	totalScrapes   prometheus.Counter
 	processMetrics map[string]*prometheus.GaugeVec
+
+	conn    net.Conn
+	url     *url.URL
+	URI     string
+	Timeout time.Duration
 }
 
 // NewExporter returns an initialized Exporter.
@@ -60,7 +132,7 @@ func (e *Exporter) scrape() {
 
 	procs, err := process.Pids()
 	if err != nil {
-		log.Errorf("Couldn't retrieve processes: %v", err)
+		logrus.Errorf("Couldn't retrieve processes: %v", err)
 		return
 	}
 
@@ -91,29 +163,29 @@ func (e *Exporter) scrape() {
 							"metric",
 						},
 					)
-					// See http://godoc.org/github.com/shirou/gopsutil/process
-					// {"rss":2514944,"vms":110858240,"shared":2113536,"text":897024,"lib":0,"data":0,"dirty":36003840}
-					// {"cpu":"cpu","user":0.0,"system":0.0,"idle":0.0,"nice":0.0,"iowait":0.0,"irq":0.0,"softirq":0.0,"steal":0.0,"guest":0.0,"guestNice":0.0,"stolen":0.0}
+
 					meminfo, _ := proc.MemoryInfoEx()
+
 					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "memory_rss"}).Set(float64(meminfo.RSS))
-					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "memory_vms"}).Set(float64(meminfo.VMS))
-					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "memory_shared"}).Set(float64(meminfo.Shared))
-					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "memory_text"}).Set(float64(meminfo.Text))
-					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "memory_lib"}).Set(float64(meminfo.Lib))
-					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "memory_data"}).Set(float64(meminfo.Data))
-					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "memory_dirty"}).Set(float64(meminfo.Dirty))
+					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "memory_locked"}).Set(float64(meminfo.Shared))
+					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "memory_shared"}).Set(float64(meminfo.VMS))
+
 					cpuinfo, _ := proc.Times()
+					cpu_percent, _ := proc.CPUPercent()
+
 					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "cpu_user"}).Set(cpuinfo.User)
 					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "cpu_system"}).Set(cpuinfo.System)
-					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "cpu_idle"}).Set(cpuinfo.Idle)
-					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "cpu_nice"}).Set(cpuinfo.Nice)
-					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "cpu_iowait"}).Set(cpuinfo.Iowait)
-					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "cpu_irq"}).Set(cpuinfo.Irq)
-					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "cpu_softirq"}).Set(cpuinfo.Softirq)
-					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "cpu_steal"}).Set(cpuinfo.Steal)
-					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "cpu_guest"}).Set(cpuinfo.Guest)
-					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "cpu_guestNice"}).Set(cpuinfo.GuestNice)
-					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "cpu_stolen"}).Set(cpuinfo.Stolen)
+					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "cpu_perc_usage"}).Set(cpu_percent)
+
+					proc_ofds, _ := proc.RlimitUsage(true)
+
+					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "proc_openfds_max"}).Set(float64(proc_ofds[process.RLIMIT_NOFILE].Hard))
+					e.processMetrics[proc_arg+strconv.Itoa(int(pid))].With(prometheus.Labels{"process": proc_arg, "pid": strconv.Itoa(int(pid)), "metric": "proc_openfds_total"}).Set(float64(proc_ofds[process.RLIMIT_NOFILE].Used))
+
+					if strings.Contains(proc_arg, "kamailio") && want_process {
+						e.getKamailioStat("core.shmmem,core.tcp_info,dlg.stats_active,ul.dump")
+					}
+
 				}
 			}
 		}
@@ -152,6 +224,188 @@ func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 	e.collectMetrics(ch)
 }
 
+// scrapeMethod will return metrics for one method.
+func (e *Exporter) scrapeMethod(method string) (map[string][]MetricValue, error) {
+	records, err := e.fetchBINRPC(method)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// we expect just 1 record of type map
+	if len(records) == 2 && records[0].Type == binrpc.TypeInt && records[0].Value.(int) == 500 {
+		return nil, fmt.Errorf(`invalid response for method "%s": [500] %s`, method, records[1].Value.(string))
+	} else if len(records) != 1 {
+		return nil, fmt.Errorf(`invalid response for method "%s", expected %d record, got %d`,
+			method, 1, len(records),
+		)
+	}
+
+	// all methods implemented in this exporter return a struct
+	items, err := records[0].StructItems()
+
+	if err != nil {
+		return nil, err
+	}
+
+	metrics := make(map[string][]MetricValue)
+
+	switch method {
+	case "tls.info":
+		fallthrough
+	case "core.shmmem":
+		fallthrough
+	case "core.tcp_info":
+		fallthrough
+	case "dlg.stats_active":
+		fallthrough
+	case "core.uptime":
+		for _, item := range items {
+			i, _ := item.Value.Int()
+			metrics[item.Key] = []MetricValue{{Value: float64(i)}}
+		}
+	case "ul.dump":
+		metrics["users_count"] = []MetricValue{{Value: float64(getRegistredUsers(items))}}
+
+	}
+	return metrics, nil
+}
+
+// fetchBINRPC talks to kamailio using the BINRPC protocol.
+func (e *Exporter) fetchBINRPC(method string) ([]binrpc.Record, error) {
+	// WritePacket returns the cookie generated
+	cookie, err := binrpc.WritePacket(e.conn, method)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// the cookie is passed again for verification
+	// we receive records in response
+	records, err := binrpc.ReadPacket(e.conn, cookie)
+
+	if err != nil {
+		return nil, err
+	}
+	return records, nil
+}
+
+func (e *Exporter) getKamailioStat(methods string) {
+
+	e.Timeout = 5
+	e.URI = "unix:/var/run/kamailio/kamailio_ctl"
+
+	var url *url.URL
+	var err error
+
+	if url, err = url.Parse(e.URI); err != nil {
+		fmt.Errorf("cannot parse URI: %w", err)
+	}
+
+	e.url = url
+
+	address := e.url.Host
+	if e.url.Scheme == "unix" {
+		address = e.url.Path
+	}
+
+	e.conn, err = net.Dial(e.url.Scheme, address)
+
+	if err != nil {
+		fmt.Printf("[%s]", err)
+	}
+
+	Methods := strings.Split(methods, ",")
+
+	e.processMetrics["kamailio"] = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "kamailio",
+			Name:      "process",
+			Help:      "metrics of the process",
+		},
+		[]string{
+			"metric",
+		},
+	)
+
+	for _, method := range Methods {
+		if _, found := metricsList[method]; !found {
+			panic("invalid method requested")
+		}
+		metricsScraped, err := e.scrapeMethod(method)
+
+		if err != nil {
+			return
+		}
+
+		for _, metricDef := range metricsList[method] {
+			metricValues, found := metricsScraped[metricDef.Name]
+
+			if !found {
+				continue
+			}
+
+			for _, metricValue := range metricValues {
+				MetricName := fmt.Sprintf("%s_%s",
+					strings.Replace(method, ".", "_", -1),
+					metricDef.Name,
+				)
+
+				e.processMetrics["kamailio"].With(prometheus.Labels{"metric": MetricName}).Set(metricValue.Value)
+			}
+		}
+	}
+
+	e.conn.Close()
+}
+
+func getRegistredUsers(items []binrpc.StructItem) int {
+	itter := 0
+	items1, _ := items[0].Value.StructItems()
+	items2, _ := items1[0].Value.StructItems()
+	items3, _ := items2[2].Value.StructItems()
+
+	for _, Info := range items3 {
+		if Info.Key != "Info" {
+			continue
+		}
+
+		Contacts, _ := Info.Value.StructItems()
+
+		for _, Contact := range Contacts {
+			if Contact.Key != "Contacts" {
+				continue
+			}
+
+			contact, _ := Contact.Value.StructItems()
+
+			for _, pre_results := range contact {
+				if pre_results.Key != "Contact" {
+					continue
+				}
+
+				results, _ := pre_results.Value.StructItems()
+
+				for _, result := range results {
+					if result.Key != "Socket" {
+						continue
+					}
+
+					if result.Value.Value != "[not set]" {
+						itter += 1
+					}
+
+				}
+
+			}
+
+		}
+
+	}
+
+	return itter
+}
+
 // Main function
 func main() {
 	const processHelpText = `Processes to (no)watch
@@ -173,36 +427,36 @@ func main() {
 		os.Exit(0)
 	}
 
-	log.Infoln("Starting process_exporter", version.Info())
-	log.Infoln("Build context", version.BuildContext())
+	logrus.Infoln("Starting process_exporter", version.Info())
+	logrus.Infoln("Build context", version.BuildContext())
 
 	// Don't split empty strings, it gives non-empty arrays?
 	if *processExpr != "" {
-		log.Infoln("Watch:", *processExpr)
+		logrus.Infoln("Watch:", *processExpr)
 		to_watch = strings.Split(*processExpr, ",")
 	} else {
-		log.Infoln("Empty list to watch")
+		logrus.Infoln("Empty list to watch")
 		to_watch = []string{}
 	}
 	if *processExprSkip != "" {
-		log.Infoln("Skip:", *processExprSkip)
+		logrus.Infoln("Skip:", *processExprSkip)
 		to_skip = strings.Split(*processExprSkip, ",")
 	} else {
-		log.Infoln("Empty list to skip")
+		logrus.Infoln("Empty list to skip")
 		to_skip = []string{}
 	}
 
 	metrics := map[string]*prometheus.GaugeVec{}
 	exporter, err := NewExporter(metrics)
 	if err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
 
 	prometheus.MustRegister(exporter)
 	prometheus.MustRegister(version.NewCollector("process_exporter"))
 
-	log.Infoln("Listening on", *listenAddress)
-	http.Handle(*metricsPath, prometheus.Handler())
+	logrus.Infoln("Listening on", *listenAddress)
+	http.Handle(*metricsPath, promhttp.Handler())
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`<html>
 		<head><title>Process Exporter</title></head>
@@ -212,6 +466,6 @@ func main() {
 		</body>
 		</html>`))
 	})
-	log.Fatal(http.ListenAndServe(*listenAddress, nil))
+	logrus.Fatal(http.ListenAndServe(*listenAddress, nil))
 
 }
